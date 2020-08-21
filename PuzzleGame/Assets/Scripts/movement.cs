837baaeaebb5ef758a07ced7c6ac6a93b0af6b4d
Saadat123456 <35307862+Saadat123456@users.noreply.github.com>
2020-08-21 20:01:23 +0500

    Script Initialized

---
+++ PuzzleGame/Assets/Scripts/movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var movementx = Input.GetAxis("Horizontal");
+        transform.position = new Vector3(movementx,0,0) * Time.deltaTime * MovementSpeed;
+
+    }
+
+    
+}
